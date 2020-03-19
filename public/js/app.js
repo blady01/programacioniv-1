a57ef8c87753e1f5dc4cae84d1c3d6f36dcded2a
vlainezt <vlainezt07@gmail.com>
2020-03-18 22:09:46 -0600

    reconfigurar elemento por nombre en script principal

--- public/js/app.js
+++ public/js/app.js
@@ -1,25 +1,27 @@
-var $ = el => document.querySelector(el);
+var $ = el => document.querySelector(el),
+    $$ = na => document.getElementsByName(na);
 document.addEventListener("DOMContentLoaded", event => {
-    let mostrarVista = $('[class*="mostrar-"]');
-    mostrarVista.addEventListener("click", e => {
-        e.preventDefault();
-        e.stopPropagation();
+    let mostrarVista = $$('navSelect');
 
-        let modulo = e.toElement.dataset.modulo;
-        //ajax general
-        fetch(`public/vistas/${modulo}/${modulo}.html`).then(resp => resp.text()).then(resp => {
-            $(`#vistas-${modulo}`).innerHTML = resp;
-
-            let btnCerrar = $(".close");
-            btnCerrar.addEventListener("click", event => {
-                $(`#vistas-${modulo}`).innerHTML = "";
-            });
-
-            let cuerpo = $("body"),
-                script = document.createElement("script");
-            script.src = `public/vistas/${modulo}/${modulo}.js`;
-            cuerpo.appendChild(script);
+    let modulo;
+    mostrarVista.forEach(element => {
+        element.addEventListener('click', e => {
+            e.stopPropagation();
+            modulo = e.toElement.dataset.modulo;
+            seleccionarPeticion(modulo);
         });
-        
     });
 });
+
+function seleccionarPeticion(modulo) {
+    fetch(`public/vistas/${modulo}/${modulo}.html`).then(resp => resp.text()).then(resp => {
+        $(`#vistas-${modulo}`).innerHTML = resp;
+        let btnCerrar = $(".close");
+        btnCerrar.addEventListener("click", event => {
+            $(`#vistas-${modulo}`).innerHTML = "";
+        });
+        let cuerpo = $("body"), script = document.createElement("script");
+        script.src = `public/vistas/${modulo}/${modulo}.js`;
+        cuerpo.appendChild(script);
+    });
+}
